abd31c27aa1fb09cd93431bfb766d72a27a97ca2
CzarneckaAnna <anna.kilyk@gmail.com>
2021-03-27 21:46:33 +0100

    Update cookie2.py
    Final code

--- cookie2.py
+++ cookie2.py
@@ -1,16 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 
-"""This project will create bot, which will play cookie game."""
-chrome_drive_path = "C:/Users/Anna/Development/chromedriver.exe"
+chrome_drive_path = YOUR_PATH
 game_page = "http://orteil.dashnet.org/experiments/cookie/"
 
 driver = webdriver.Chrome(chrome_drive_path)
 driver.get(game_page)
 
 # the game (game_time) lasts 5 minutes, every 5 seconds bot will buy the most expensive, affordable upgrade.
-game_time = time.time() + 1 * 60
+game_time = time.time() + 5 * 60
 buy_time = time.time() + 5
 
 cookie_click = driver.find_element_by_id("cookie")
@@ -23,18 +21,20 @@ while True:
     cookie_click.click()
 
     if time.time() >= buy_time:
-        money = driver.find_element_by_id("money").text.replace(",", "")
-        buy_time += 5
-        print(f"end game:{game_time}")
-        print(f"time: {time.time()}")
+        money = int(driver.find_element_by_id("money").text.replace(",", ""))
 
-    # TODO 4: Every 5 seconds, check if it's possible to buy upgrade - if yes, select the most expensive from available.
+        for price in range(len(upgrades_list) - 1):
+            item_price = int(upgrades_list[price][1].replace(",", ""))
+            if money >= item_price:
+                upgrade = upgrades_list[price][0]
+
+        if upgrade != "":
+            selected_upgrade = driver.find_element_by_id(f"buy{upgrade}")
+            selected_upgrade.click()
+
+        buy_time += 5
 
     if time.time() > game_time:
         results = driver.find_element_by_id("cps").text
         print(results)
         break
-
-
-
-
