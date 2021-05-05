2df950c6e27cf629306a9904bda409ccfb53378a
CzarneckaAnna <anna.kilyk@gmail.com>
2021-05-05 10:32:30 +0200

    Create adding_followers_to_instagram_account.py
    This program will fallow all followers from selected account (SIMILAR_ACCOUNT) - initial commit

---
+++ adding_followers_to_instagram_account.py
@@ -0,0 +1,70 @@
+"""This program will fallow all followers from selected account (SIMILAR_ACCOUNT)"""
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+INSTAGRAM_URL = "https://www.instagram.com/"
+"""Update with you personal credential"""
+CHROME_DRIVE_PATH = USER_CHROM_PATH
+INSTAGRAM_LOGIN = USER_INSTAGRAM_LOGIN
+INSTAGRAM_PASSWORD = USER_INSTAGRAM_PASSWORD
+SIMILAR_ACCOUNT = ACCOUNT_SELECTED_BY_USER
+
+
+class InstaFollower:
+    def __init__(self):
+        self.driver = webdriver.Chrome(CHROME_DRIVE_PATH)
+
+    def login(self):
+        page = self.driver
+        page.get(INSTAGRAM_URL)
+        wait = WebDriverWait(page, 10)
+        waiting_1 = wait.until(EC.invisibility_of_element_located((By.ID, "loginForm")))
+
+        # cookies accept
+        cookies = page.find_element_by_xpath("/html/body/div[2]/div/div/button[1]").click()
+
+        waiting_1
+        # login to instagram
+        login = page.find_element_by_name("username")
+        password = page.find_element_by_name("password")
+
+        login.send_keys(INSTAGRAM_LOGIN)
+        password.send_keys(INSTAGRAM_PASSWORD)
+
+        password.send_keys(Keys.ENTER)
+
+        # information pop-aps:
+        waiting_2 = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@id='react-root']/section/main/div/div")))
+        save_login_data = page.find_element_by_xpath("//*[@id='react-root']/section/main/div/div/div/div/button").click()
+
+        waiting_3 = wait.until(EC.presence_of_all_elements_located((By.XPATH, "/html/body/div[4]/div/div/div")))
+        notification_off = page.find_element_by_xpath("/html/body/div[4]/div/div/div/div[3]/button[2]").click()
+
+    def find_followers(self):
+        selected_page = self.driver
+        selected_page.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
+
+
+        # wait = WebDriverWait(selected_page, 10)
+        #
+        # waiting_4 = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@id='react-root']/section/main/div/ul/li[2]")))
+
+            #followers = selected_page.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a").click()
+
+    def follow(self):
+        pass
+
+
+inst_page = InstaFollower()
+inst_page.login()
+inst_page.find_followers()
+inst_page.follow()
+
+
+
+
